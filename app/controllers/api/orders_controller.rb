171d1e7d3d63289665bbcf11e2cb13ca4b959c5f
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-10 13:49:10 -0500

    Working views in class as cohort

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -8,20 +8,28 @@ class Api::OrdersController < ApplicationController
     end
   end
 
-  def show
-    if current_user
-      order_id = params["id"]
-      @order = Order.find_by(id: order_id)
-      render "show.json.jb"
-    else
-      render json: { errors: @order.errors.full_messages }, status: 406
-    end
+  def show #did in class
+    order_id = params[:id]
+    @order = current_user.orders.find(order_id)
+    render "show.json.jb"
   end
 
+  # def show
+  #   if current_user
+  #     order_id = params["id"]
+  #     @order = Order.find_by(id: order_id)
+  #     render "show.json.jb"
+  #   else
+  #     render json: { errors: @order.errors.full_messages }, status: 406
+  #   end
+  # end
+
   def create
+    product = Product.find_by(id: params[:product_id])
+
     if current_user
       @order = Order.new(
-        user_id: current_user[:id],
+        user_id: current_user.id,
         product_id: params[:product_id],
         quantity: params[:quantity],
       )
