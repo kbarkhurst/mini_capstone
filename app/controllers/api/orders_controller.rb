4a095e36acf6703317195689a1077000e2448e83
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-09 20:16:32 -0500

    Adds current_user id for creating orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,8 +1,18 @@
 class Api::OrdersController < ApplicationController
   def create
+    # @order = Order.new(
+    #   user_id: 1, #can only figure out hard-coding the userid number
+    #   product_id: params[:product_id],
+    #   quantity: params[:quantity],
+    # )
+    # if @order.save
+    #   render "show.json.jb"
+    # else
+    #   render json: { errors: @order.errors.full_messages }, status: 406
+    # end
     if current_user
       @order = Order.new(
-        # user_id: user.id,
+        user_id: current_user[:id],
         product_id: params[:product_id],
         quantity: params[:quantity],
       )
